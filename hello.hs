86b20bbe28153a950e7d3344db136b617303beb6
CENGIZ GUERTUSGIL <cengiz.guertusgil@gmail.com>
2023-06-09 17:42:05 +0200

    create basic functions to generate html

--- hello.hs
+++ hello.hs
@@ -1,1 +1,16 @@
-main = putStrLn "hi"
+main = putStrLn myHtml
+
+-- function call does not need parenthesis
+myHtml = makeHtml "My page title" "My page content"
+
+-- <> is used to concat the strings
+-- <> is an infix operator
+html_ input = "<html>" <> input <> "</html>"
+
+body_ input = "<body>" <> input <> "</body>"
+
+head_ input = "<head>" <> input <> "</head>"
+
+title_ input = "<title>" <> input <> "</title>"
+
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
