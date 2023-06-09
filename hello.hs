9e7c2767f807452175b420fa752c2af48a29dd6b
CENGIZ GUERTUSGIL <cengiz.guertusgil@gmail.com>
2023-06-09 18:01:49 +0200

    wip

--- hello.hs
+++ hello.hs
@@ -5,12 +5,29 @@ myHtml = makeHtml "My page title" "My page content"
 
 -- <> is used to concat the strings
 -- <> is an infix operator
-html_ input = "<html>" <> input <> "</html>"
+html_ :: String -> String
+html_ val = "<html>" <> val <> "</html>"
 
-body_ input = "<body>" <> input <> "</body>"
+body_ :: String -> String
+body_ val = "<body>" <> val <> "</body>"
 
-head_ input = "<head>" <> input <> "</head>"
+head_ :: String -> String
+head_ val = "<head>" <> val <> "</head>"
 
-title_ input = "<title>" <> input <> "</title>"
+title_ :: String -> String
+title_ val = "<title>" <> val <> "</title>"
 
+-- in haskell functions actually only take 1 string. it seems like 2
+-- makeHtml is a function that takes one string argument and returns a function. 
+-- The function it returns takes a string argument as well and finally returns a string.
+
+-- the arrow -> is right associative so String -> String -> String
+--  becomes String -> (String -> String)
+makeHtml :: String -> String -> String
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
+
+el :: String -> String -> String
+el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+-- html1_ el "html"
